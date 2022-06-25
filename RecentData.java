3dd12af5613fc882dfac0a350367c25925c093e4
Ashutosh Priyadarshi <102952424+Ash-0601@users.noreply.github.com>
2022-06-25 20:37:08 +0530

    Create RecentData.java

---
+++ RecentData.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.model;
+
+public class RecentsData {
+    String placename;
+    String countryname;
+    String price;
+
+    public RecentsData(String placename,String countryname,String price) {
+        this.placename = placename;
+        this.countryname=countryname;
+        this.price=price;
+    }
+}
