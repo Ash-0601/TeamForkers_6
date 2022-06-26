bb5232260cd29899ed25e44c32431ab2bf26d54d
Ashutosh Priyadarshi <102952424+Ash-0601@users.noreply.github.com>
2022-06-26 16:10:12 +0530

    Update RecentAdapter.java

--- RecentAdapter.java
+++ RecentAdapter.java
@@ -4,28 +4,38 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.uttrakhandtoruism.R;
-import com.example.uttrakhandtoruism.model.RecentsData;
+
+
+
+import com.example.dreamtravel.R;
+import com.example.dreamtravel.model.RecentData;
 
 import java.util.List;
 
-public class RecentsAdapter extends RecyclerView.Adapter<RecentsAdapter.RecentViewHolder> {
+public class RecentAdapter extends RecyclerView.Adapter<RecentAdapter.RecentViewHolder> {
+
     Context context;
-    List<RecentsData>recentsDataList;
+    List<RecentData>recentsDataList;
 
     @NonNull
     @Override
     public RecentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_main,parent,false);
         return new RecentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecentViewHolder holder, int position) {
+        holder.countryname.setText(recentsDataList.get(position).getCountryname());
+        holder.placename.setText(recentsDataList.get(position).getPlacename());
+        holder.state.setText(recentsDataList.get(position).getState());
+        holder.imageView2.setImageResource(recentsDataList.get(position).getImageurl());
 
     }
 
@@ -35,8 +45,15 @@ public class RecentsAdapter extends RecyclerView.Adapter<RecentsAdapter.RecentVi
     }
 
     public static final class RecentViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView2;
+        TextView placename,countryname,state;
         public RecentViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView2=itemView.findViewById(R.id.imageView);
+            placename=itemView.findViewById(R.id.textView);
+            countryname=itemView.findViewById(R.id.textView);
+            state=itemView.findViewById(R.id.textView);
+
         }
     }
 }
